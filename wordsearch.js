77513fa7b93f52c861c66db768db96566d8c0274
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-15 21:10:58 +0000

    a function that receives a 2D array of letters and word and returns true if the word is found, return false if the word is not found

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    // join the horizontal array 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    // console.log(horizontalJoin);
+    // horizontal one includes the word = true
+    for (let l of horizontalJoin) {
+        if (l.includes(word))
+            return true;
     }
+    // vertical one includes the word = true
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+    for (let l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
 }
 
+// yesterday's work
+const transpose = function (matrix) {
+    let result = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        let column = [];
+        for (let j = 0; j < matrix.length; j++) {     
+            column.push(matrix[j][i]);
+        }
+        console.log(column);
+        result.push(column);
+    }
+    return result;
+};
+
+
+//example
 module.exports = wordSearch
