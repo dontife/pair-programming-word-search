36078c79d697a2920a7257e3137b75475cd717e8
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-08 22:29:08 +0000

    a function that receives a 2D array of letters and a word and checks to find the word horizontally and vertically, returns true if the word is found and false vice versa

--- wordsearch.js
+++ wordsearch.js
@@ -1,36 +1,38 @@
-const wordSearch = (letters, word) => {
-    // join the horizontal array 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    // console.log(horizontalJoin);
-    // horizontal one includes the word = true
-    for (let l of horizontalJoin) {
-        if (l.includes(word))
-            return true;
+const transpose = (matrix) => {
+    let output = [];
+    if (!matrix.length) {
+      return output;
     }
-    // vertical one includes the word = true
-    const verticalJoin = transpose(letters).map(ls => ls.join(''))
-    for (let l of verticalJoin) {
-        if (l.includes(word)) {
-            return true;
-        }
+    
+    output = matrix[0].map((column, i) => {
+      return matrix.map((row, j) => {
+        return matrix[j][i];
+      });
+    });
+    return output;
+  };
+  
+  const wordSearch = (letters, word) => {
+    if (!letters.length) {
+      return false;
     }
+    const transposed = transpose(letters);
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const reverseHorizontally = letters.map(ls => ls.slice().reverse().join(''));
+    const reverseVertically = transposed.map(ls => ls.slice().reverse().join(''));
+    const join = transposed.map(ls => ls.join(''));
+      
+    let array = [horizontalJoin, join, reverseHorizontally, reverseVertically];
+  
+    for (let i = 0; i < array.length; i++) {
+      for (let l of array[i]) {
+        if (l.includes(word)) return true;
+      }
+    }
+      
     return false;
-}
-
-// yesterday's work
-const transpose = function (matrix) {
-    let result = [];
-    for (let i = 0; i < matrix[0].length; i++) {
-        let column = [];
-        for (let j = 0; j < matrix.length; j++) {     
-            column.push(matrix[j][i]);
-        }
-        console.log(column);
-        result.push(column);
-    }
-    return result;
-};
-
-
-//example
-module.exports = wordSearch
+  
+  };
+  
+  module.exports = wordSearch;
+  
